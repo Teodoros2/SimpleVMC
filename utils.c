9632c22618092548f9d65f9a89a96c0c0b67d8f8
Teodoros2 <186217124+Teodoros2@users.noreply.github.com>
2024-11-01 00:00:00 +0500

    'add file utils.c'

---
+++ utils.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+// You can add utility functions here as needed
+// For now, this file can remain empty or be used for future enhancements.
